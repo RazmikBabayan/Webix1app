3c97a20e9a09a199415918dc3217e52192ae4c81
Razmik <mrrazmik8@gmail.com>
2022-01-20 01:32:40 +0300

    Update datatable.js

--- js/datatable.js
+++ js/datatable.js
@@ -1,6 +1,6 @@
 const datatable = {
   view: "datatable",
-  css:"webix_data_border webix_header_border",
+  css:"webix_data_border webix_header_border position: absolute; z-index: 3; width: 100%; height: auto;top: 0; left: 0; transition: opacity .4s ease-in-out;",
   autoConfig: true,
   autoheight: true,
   dragColumn: true,
